4c785b869d3bbabfe9ac7d04bf70109995c0fca3
Usama Ejaz <usama.ejaz@pf.com.pk>
2020-02-14 19:48:12 +0500

    Screen Share Audio Fix

--- components/ot-publisher.vue
+++ components/ot-publisher.vue
@@ -56,33 +56,70 @@ export default {
     watch: {
     // whenever options changes, this function will run
     opts: function () {
-      this.session.unpublish(this.publisher);
-      this.publisher = OT.initPublisher(this.$el, this.opts, (err) => {
-        if (err) {
-          this.$emit('error', err);
-        } else {
-          this.$emit('publisherCompleted');
-        }
-      });
-      console.log("In wathcer");
-      console.log(this.publisher);
+        this.session.unpublish(this.publisher);
+        if(this.opts.videoSource === 'screen'){
+            Promise.all([
+                OT.getUserMedia({
+                    videoSource: 'screen'
+                }),
+                OT.getUserMedia({
+                    videoSource: null
+                })
+            ]).then(([screenStream, micStream]) => {
+               this.publisher = OT.initPublisher(this.$el, {
+                    videoSource: screenStream.getVideoTracks()[0],
+                    audioSource: micStream.getAudioTracks()[0]
+                },  (err) => {
+                    if (err) {
+                        this.$emit('error', err);
+                    } else {
+                        this.$emit('publisherCompleted');
+                    }
+                });
 
-      this.$emit('publisherCreated', this.publisher);
-      const publish = () => {
-        this.session.publish(this.publisher, (err) => {
-          if (err) {
-            this.$emit('error', err);
-          } else {
-            this.$emit('publisherConnected', this.publisher);
-          }
-        });
-      };
-      if (this.session && this.session.isConnected()) {
-        publish()
-      }
-      if (this.session) {
-        this.session.on('sessionConnected', publish)
-      }
+              this.$emit('publisherCreated', this.publisher);
+              const publish = () => {
+                  this.session.publish(this.publisher, (err) => {
+                      if (err) {
+                          this.$emit('error', err);
+                      } else {
+                          this.$emit('publisherConnected', this.publisher);
+                      }
+                  });
+              };
+              if (this.session && this.session.isConnected()) {
+                  publish()
+              }
+              if (this.session) {
+                  this.session.on('sessionConnected', publish)
+              }
+            });
+        }else {
+            this.publisher = OT.initPublisher(this.$el, this.opts, (err) => {
+                if (err) {
+                    this.$emit('error', err);
+                } else {
+                    this.$emit('publisherCompleted');
+                }
+            });
+
+            this.$emit('publisherCreated', this.publisher);
+            const publish = () => {
+                this.session.publish(this.publisher, (err) => {
+                    if (err) {
+                        this.$emit('error', err);
+                    } else {
+                        this.$emit('publisherConnected', this.publisher);
+                    }
+                });
+            };
+            if (this.session && this.session.isConnected()) {
+                publish()
+            }
+            if (this.session) {
+                this.session.on('sessionConnected', publish)
+            }
+        }
     }
   },
 }
